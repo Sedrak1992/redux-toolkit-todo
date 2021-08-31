7f81b538664368d3d044dcff061d56f1b61b814e
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2021-08-31 11:25:39 +0400

    a

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addtodo, decrement, increment } from "./counterSlice";
+import AddIcon from "@material-ui/icons/Add";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 export function Counter() {
   const [value, setInputValue] = useState("");
@@ -10,21 +12,32 @@ export function Counter() {
   const hendleAddTodo = () => {
     dispatch(addtodo(value));
     setInputValue("");
-    if(value == "") {
-        alert("g;'ilh")
+    if (value == "") {
+      alert("g;'ilh");
     }
   };
-console.log(value);
+  const deleteTodo = (i) => {
+    setInputValue((count) =>
+    count.filter((item, index) => index !== i)
+    );
+  };
+  console.log(value);
   return (
     <div>
-      <div style={{marginTop:"50px"}}>
-        <input value={value} onChange={(e) => setInputValue(e.target.value)} />
+      <div style={{ marginTop: "50px" }}>
+        <input
+          value={value}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
         <button aria-label="Increment value" onClick={hendleAddTodo}>
-          Increment
+          add....
         </button>
         <div style={{ border: "1px red solid" }}>
           {count.map((item, i) => (
-            <div key={i}> {item}</div>
+            <div key={i}>
+              {" "}
+              {item} <DeleteIcon onClick={deleteTodo} />
+            </div>
           ))}
         </div>
       </div>
